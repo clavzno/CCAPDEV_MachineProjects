9ae7b1e9c2d3ddb35526e79a00add6e4bef56496
GitBl1tz <gerome_ranigo@dlsu.edu.ph>
2024-03-10 14:17:13 +0800

    copy

---
+++ phase2_edit copy/controllers/authController.js
@@ -0,0 +1,77 @@
+// authController.js
+
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const User = require('../models/userModel.js');
+
+// Register a new user
+exports.signup = async (req, res) => {
+  try {
+    const { // Extract user data from request body
+        firstName, 
+        lastName, 
+        email, 
+        password, 
+        birthdate, 
+        gender 
+    } = req.body;
+
+    // Check if email already exists
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: 'User with this email already exists' });
+    }
+
+    // Hash the password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Create a new user
+    const newUser = new User({
+      firstName,
+      lastName,
+      email,
+      password: hashedPassword,
+      birthdate,
+      gender
+    });
+    await newUser.save();
+
+    res.status(201).json({ message: 'User registered successfully' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+};
+
+
+
+
+
+// Login a user
+exports.login = async (req, res) => {
+  try {
+    // Extract login credentials from request body
+    const { email, password } = req.body;
+
+    // Find the user by email
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    // Check if the password is correct
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: 'Invalid password' });
+    }
+
+    // Generate a JWT token
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+
+    res.status(200).json({ token });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+};
---
+++ phase2_edit copy/controllers/mainController.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose'); // Importing mongoose module
+
+
+
+// Will continue
+
+
+
+
+module.exports = mainController;
