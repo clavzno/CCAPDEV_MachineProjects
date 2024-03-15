734194308b3893657c05aae45d3e65d682bd133b
Shinoruba <homssi002@gmail.com>
2024-03-15 23:52:24 +0800

    asdasdasdas

--- phase2_edit/models/postModel.js
+++ phase2_edit/models/postModel.js
@@ -18,7 +18,7 @@ const postSchema = new mongoose.Schema({
     user: {
       type: mongoose.Schema.Types.ObjectId,
       ref: 'User',
-      required: true
+      required: false
     },
     postDate: {
       type: Date, 
