b4fc59d7e2549c7e5290789aed6cf522ee6dea78
Shinoruba <homssi002@gmail.com>
2024-03-31 16:56:53 +0800

    [USELESS] files useless, may or may not delete

--- CCAPDEV-Phase3-Group12/public/js/login.js
+++ CCAPDEV-Phase3-Group12/public/js/login.js
@@ -8,33 +8,33 @@
 
 */
 
-$(document).ready(function() {
-    // Function to handle form submission
-    $("#loginForm").submit(function(event) {
-      event.preventDefault(); // Prevent the form from submitting normally
+// $(document).ready(function() {
+//     // Function to handle form submission
+//     $("#loginForm").submit(function(event) {
+//       event.preventDefault(); // Prevent the form from submitting normally
   
-      // Get the values from the form fields
-      var username = $("#username").val();
-      var password = $("#password").val();
-      var rememberMe = $("#rememberMe").is(":checked");
+//       // Get the values from the form fields
+//       var username = $("#username").val();
+//       var password = $("#password").val();
+//       var rememberMe = $("#rememberMe").is(":checked");
   
-      // Send the login request to the server
-      $.ajax({
-        url: "/login",
-        type: "POST",
-        data: {
-            username: username,
-          password: password,
-          rememberMe: rememberMe
-        },
-        success: function(response) {
-          console.log("Login successful!"); // Handle successful login response
-          window.location.href = "../views/feed.hbs"; // Replace with actual dashboard URL
-        },
-        error: function(xhr, status, error) {    
-          console.error("Login failed:", error); // Handle login error
-          alert("Login failed. Please check your credentials and try again."); // Display an error message to the user
-        }
-      });
-    });
-  });
+//       // Send the login request to the server
+//       $.ajax({
+//         url: "/login",
+//         type: "POST",
+//         data: {
+//             username: username,
+//           password: password,
+//           rememberMe: rememberMe
+//         },
+//         success: function(response) {
+//           console.log("Login successful!"); // Handle successful login response
+//           window.location.href = "../views/feed.hbs"; // Replace with actual dashboard URL
+//         },
+//         error: function(xhr, status, error) {    
+//           console.error("Login failed:", error); // Handle login error
+//           alert("Login failed. Please check your credentials and try again."); // Display an error message to the user
+//         }
+//       });
+//     });
+//   });
