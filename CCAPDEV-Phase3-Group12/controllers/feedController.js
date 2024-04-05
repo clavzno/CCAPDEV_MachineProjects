6851e8c1058a2926d6b839b218c12531858d8435
GitBl1tz <gerome_ranigo@dlsu.edu.ph>
2024-04-05 22:39:37 +0800

    letsgetit

--- CCAPDEV-Phase3-Group12/controllers/feedController.js
+++ CCAPDEV-Phase3-Group12/controllers/feedController.js
@@ -76,7 +76,8 @@ const feedController = {
       post.postLikes.push(user);
       await post.save();
       console.log(post);
-      res.render('feed', {post:post, input:true});
+      res.redirect('back');
+      //res.render('feed', {post:post, input:true});
     }catch(err){
       console.error(err);
       return res.status(500).json({ message: 'Server error' });
